10c860067e06d3b2409cad8bcbbe4ebca82f51bb
ssw218 <lyklyk218@gmail.com>
2015-01-09 18:17:30 +0800

    2015/1/9 18:17

--- gen/com/example/tutorialanimation/R.java
+++ gen/com/example/tutorialanimation/R.java
@@ -233,32 +233,6 @@ containing a value of this type.
 or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
          */
         public static final int activityChooserViewStyle=0x7f010034;
-        /** <p>May be an integer value, such as "<code>100</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-<p>May be one of the following constant values.</p>
-<table>
-<colgroup align="left" />
-<colgroup align="left" />
-<colgroup align="left" />
-<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
-<tr><td><code>ink_delete</code></td><td>1</td><td></td></tr>
-<tr><td><code>ink_erase_all</code></td><td>2</td><td></td></tr>
-<tr><td><code>ink_return</code></td><td>3</td><td></td></tr>
-<tr><td><code>ink_tab</code></td><td>4</td><td></td></tr>
-<tr><td><code>typeset_delete</code></td><td>5</td><td></td></tr>
-<tr><td><code>typeset_insert_space</code></td><td>6</td><td></td></tr>
-<tr><td><code>typeset_erase_all</code></td><td>7</td><td></td></tr>
-<tr><td><code>typeset_return</code></td><td>8</td><td></td></tr>
-<tr><td><code>typeset_tab</code></td><td>9</td><td></td></tr>
-<tr><td><code>typeset_select_text</code></td><td>10</td><td></td></tr>
-<tr><td><code>typeset_insert_text</code></td><td>11</td><td></td></tr>
-</table>
-         */
-        public static final int animation=0x7f0100a9;
         /**  Specifies a background drawable for the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
 or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
@@ -1672,79 +1646,68 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020033;
     }
     public static final class id {
-        public static final int action_bar=0x7f0a003c;
+        public static final int action_bar=0x7f0a0031;
         public static final int action_bar_activity_content=0x7f0a0003;
-        public static final int action_bar_container=0x7f0a003b;
-        public static final int action_bar_root=0x7f0a0037;
+        public static final int action_bar_container=0x7f0a0030;
+        public static final int action_bar_root=0x7f0a002c;
         public static final int action_bar_spinner=0x7f0a0002;
-        public static final int action_bar_subtitle=0x7f0a002a;
-        public static final int action_bar_title=0x7f0a0029;
-        public static final int action_context_bar=0x7f0a003d;
+        public static final int action_bar_subtitle=0x7f0a001f;
+        public static final int action_bar_title=0x7f0a001e;
+        public static final int action_context_bar=0x7f0a0032;
         public static final int action_menu_divider=0x7f0a0005;
         public static final int action_menu_presenter=0x7f0a0006;
-        public static final int action_mode_bar=0x7f0a0039;
-        public static final int action_mode_bar_stub=0x7f0a0038;
-        public static final int action_mode_close_button=0x7f0a002b;
-        public static final int activity_chooser_view_content=0x7f0a002c;
+        public static final int action_mode_bar=0x7f0a002e;
+        public static final int action_mode_bar_stub=0x7f0a002d;
+        public static final int action_mode_close_button=0x7f0a0020;
+        public static final int activity_chooser_view_content=0x7f0a0021;
         public static final int always=0x7f0a0014;
         public static final int beginning=0x7f0a001b;
-        public static final int checkbox=0x7f0a0034;
+        public static final int checkbox=0x7f0a0029;
         public static final int collapseActionView=0x7f0a0015;
-        public static final int content_fragment=0x7f0a004c;
-        public static final int decor_content_parent=0x7f0a003a;
-        public static final int default_activity_button=0x7f0a002f;
+        public static final int content_fragment=0x7f0a0041;
+        public static final int decor_content_parent=0x7f0a002f;
+        public static final int default_activity_button=0x7f0a0024;
         public static final int dialog=0x7f0a0019;
         public static final int disableHome=0x7f0a000d;
         public static final int dropdown=0x7f0a001a;
-        public static final int edit_query=0x7f0a003e;
+        public static final int edit_query=0x7f0a0033;
         public static final int end=0x7f0a001c;
-        public static final int expand_activities_button=0x7f0a002d;
-        public static final int expanded_menu=0x7f0a0033;
+        public static final int expand_activities_button=0x7f0a0022;
+        public static final int expanded_menu=0x7f0a0028;
         public static final int home=0x7f0a0000;
         public static final int homeAsUp=0x7f0a000e;
-        public static final int icon=0x7f0a0031;
+        public static final int icon=0x7f0a0026;
         public static final int ifRoom=0x7f0a0016;
-        public static final int image=0x7f0a002e;
-        public static final int index_fragment=0x7f0a004b;
-        public static final int ink_delete=0x7f0a001e;
-        public static final int ink_erase_all=0x7f0a001f;
-        public static final int ink_return=0x7f0a0020;
-        public static final int ink_tab=0x7f0a0021;
-        public static final int linear_layout=0x7f0a004a;
+        public static final int image=0x7f0a0023;
+        public static final int index_fragment=0x7f0a0040;
+        public static final int linear_layout=0x7f0a003f;
         public static final int listMode=0x7f0a000a;
-        public static final int list_item=0x7f0a0030;
+        public static final int list_item=0x7f0a0025;
         public static final int middle=0x7f0a001d;
         public static final int never=0x7f0a0017;
         public static final int none=0x7f0a000f;
         public static final int normal=0x7f0a000b;
         public static final int progress_circular=0x7f0a0007;
         public static final int progress_horizontal=0x7f0a0008;
-        public static final int radio=0x7f0a0036;
-        public static final int search_badge=0x7f0a0040;
-        public static final int search_bar=0x7f0a003f;
-        public static final int search_button=0x7f0a0041;
-        public static final int search_close_btn=0x7f0a0046;
-        public static final int search_edit_frame=0x7f0a0042;
-        public static final int search_go_btn=0x7f0a0048;
-        public static final int search_mag_icon=0x7f0a0043;
-        public static final int search_plate=0x7f0a0044;
-        public static final int search_src_text=0x7f0a0045;
-        public static final int search_voice_btn=0x7f0a0049;
-        public static final int shortcut=0x7f0a0035;
+        public static final int radio=0x7f0a002b;
+        public static final int search_badge=0x7f0a0035;
+        public static final int search_bar=0x7f0a0034;
+        public static final int search_button=0x7f0a0036;
+        public static final int search_close_btn=0x7f0a003b;
+        public static final int search_edit_frame=0x7f0a0037;
+        public static final int search_go_btn=0x7f0a003d;
+        public static final int search_mag_icon=0x7f0a0038;
+        public static final int search_plate=0x7f0a0039;
+        public static final int search_src_text=0x7f0a003a;
+        public static final int search_voice_btn=0x7f0a003e;
+        public static final int shortcut=0x7f0a002a;
         public static final int showCustom=0x7f0a0010;
         public static final int showHome=0x7f0a0011;
         public static final int showTitle=0x7f0a0012;
         public static final int split_action_bar=0x7f0a0004;
-        public static final int submit_area=0x7f0a0047;
+        public static final int submit_area=0x7f0a003c;
         public static final int tabMode=0x7f0a000c;
-        public static final int title=0x7f0a0032;
-        public static final int typeset_delete=0x7f0a0022;
-        public static final int typeset_erase_all=0x7f0a0023;
-        public static final int typeset_insert_space=0x7f0a0024;
-        public static final int typeset_insert_text=0x7f0a0025;
-        public static final int typeset_return=0x7f0a0026;
-        public static final int typeset_select_text=0x7f0a0027;
-        public static final int typeset_tab=0x7f0a0028;
+        public static final int title=0x7f0a0027;
         public static final int up=0x7f0a0001;
         public static final int useLogo=0x7f0a0013;
         public static final int withText=0x7f0a0018;
@@ -3107,51 +3070,6 @@ containing a value of this type.
           @attr name com.example.tutorialanimation:topBottomBarArrowSize
         */
         public static final int DrawerArrowToggle_topBottomBarArrowSize = 4;
-        /** Attributes that can be used with a InkGestureAnimation.
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #InkGestureAnimation_animation com.example.tutorialanimation:animation}</code></td><td></td></tr>
-           </table>
-           @see #InkGestureAnimation_animation
-         */
-        public static final int[] InkGestureAnimation = {
-            0x7f0100a9
-        };
-        /**
-          <p>This symbol is the offset where the {@link com.example.tutorialanimation.R.attr#animation}
-          attribute's value can be found in the {@link #InkGestureAnimation} array.
-
-
-          <p>May be an integer value, such as "<code>100</code>".
-<p>This may also be a reference to a resource (in the form
-"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
-theme attribute (in the form
-"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
-containing a value of this type.
-<p>May be one of the following constant values.</p>
-<table>
-<colgroup align="left" />
-<colgroup align="left" />
-<colgroup align="left" />
-<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
-<tr><td><code>ink_delete</code></td><td>1</td><td></td></tr>
-<tr><td><code>ink_erase_all</code></td><td>2</td><td></td></tr>
-<tr><td><code>ink_return</code></td><td>3</td><td></td></tr>
-<tr><td><code>ink_tab</code></td><td>4</td><td></td></tr>
-<tr><td><code>typeset_delete</code></td><td>5</td><td></td></tr>
-<tr><td><code>typeset_insert_space</code></td><td>6</td><td></td></tr>
-<tr><td><code>typeset_erase_all</code></td><td>7</td><td></td></tr>
-<tr><td><code>typeset_return</code></td><td>8</td><td></td></tr>
-<tr><td><code>typeset_tab</code></td><td>9</td><td></td></tr>
-<tr><td><code>typeset_select_text</code></td><td>10</td><td></td></tr>
-<tr><td><code>typeset_insert_text</code></td><td>11</td><td></td></tr>
-</table>
-          @attr name com.example.tutorialanimation:animation
-        */
-        public static final int InkGestureAnimation_animation = 0;
         /** Attributes that can be used with a LinearLayoutCompat.
            <p>Includes the following attributes:</p>
            <table>
